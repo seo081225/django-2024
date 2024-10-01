1ad79a978673dbbdc70108ceafc7e1d73d476f47
seo <seo081225@gmail.com>
2024-10-01 10:28:13 +0900

    Assignment 25 add likes count

--- tweets/models.py
+++ tweets/models.py
@@ -10,11 +10,16 @@ class BaseModel(models.Model):
 class Tweet(BaseModel):
     payload = models.TextField(max_length=180)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return self.payload
+        return f"Tweet(payload={self.payload} / like_count={self.likes.count()} / author={self.user.pk};{self.user.username})"
+    
+    def total_likes_count(self):
+        return self.likes.count()
     
 class Like(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name="likes")
+
     def __str__(self):
         return f"{self.user.username} liked '{self.tweet.payload}'"
